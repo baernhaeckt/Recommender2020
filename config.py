dde40ebcc5e12c130ddc3b18e66bcb82577ce9bb
Oliver Hofmann <oliver.hofmann@ti8m.ch>
2020-09-05 18:09:34 +0200

    Add paid offers url.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     OFFERS_URL = "https://baernhaeckt2020.azurewebsites.net/api/Offers"
-    PAID_OFFERS_URL = ""
+    PAID_OFFERS_URL = "https://baernhaeckt2020.azurewebsites.net/api/PaidOffers"
 
     OFFERS_FILENAME = "offers.csv"
     PAID_OFFERS_FILENAME = "paidoffers.csv"
